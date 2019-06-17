c8d00ddd489675e330627e09768468ec76bfe033
JP <jayhpatel1201@gmail.com>
2019-06-17 18:01:16 -0400

    changed css and javascript file
    Animations for circles, boxes , circle to box and box to circle

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,27 @@
 $(document).ready(function() {
+ //Circle
+  $('.circle').on('click', function() {
+    $(this).toggleClass('big');
+  });
 
+  $('.circle').on('mouseenter', function() {
+    $(this).addClass('spin');
+  });
+  //Box
+  $('.box').on('click', function() {
+    $(this).toggleClass('big');
+  });
 
+  $('.box').on('mouseenter', function() {
+    $(this).addClass('spin');
+  });
+
+  //Box to circle and Circle to box
+  $('.boxtocircle').on('mouseenter', function() {
+    $(this).addClass('change-shape');
+  });
+
+  $('.circletobox').on('mouseenter', function() {
+    $(this).addClass('change-shape');
+  });
 });
